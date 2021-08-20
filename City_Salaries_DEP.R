363e2249774e140f68657365047ae545d6391c7c
The H Project <harris.lam@gmail.com>
2021-08-20 07:50:21 -0400

    quick export

--- City_Salaries_DEP.R
+++ City_Salaries_DEP.R
@@ -2,9 +2,9 @@
 library(bit64)
 library(data.table)
 
-inputdirectory <- 'C:/Data/'
-inputfile <- 'Citywide_Payroll_20210810.RDS'
+inputdirectory <- 'd:/Data/'
+inputfile <- 'Citywide_Payroll_DEP_20210810.RDS'
 
 dataset <- fread(paste0(inputdirectory, inputfile))
 
-DEP_dataset <- dataset[`Payroll Number` == 826]
+
--- City_Salaries_to_RDS.R
+++ City_Salaries_to_RDS.R
@@ -4,13 +4,54 @@
 #
 library(bit64)
 library(data.table)
+library(openxlsx)
 
-inputdirectory <- 'C:/Data/'
-inputfile <- 'Citywide_Payroll_Data__Fiscal_Year_20210810.csv'
+inputdirectory <- 'D:/Data/'
+outputdirectory <- 'D:/Data/'
+inputfile <- 'Citywide_Payroll_Data_Fiscal_Year_20210810.csv'
 outputfile <- 'Citywide_Payroll_20210810.RDS'
+outputprefix <- 'CP_20210810_'
 
 dataset <- fread(paste0(inputdirectory, inputfile))
 
 saveRDS(dataset, paste0(inputdirectory, outputfile))
 
-test <- dataset[`Last Name` == 'LAM']
+test <- dataset[`Last Name` == 'WILSON' & `First Name` == 'PETA GAY']
+
+DEP_dataset <- dataset[`Payroll Number` == 826]
+
+
+# Create worksheet format styles
+# Some formatting afterwards.
+Style0 <- createStyle(numFmt = '0')
+StyleH <- createStyle(textDecoration = 'bold', fontColour = 'white', bgFill = 'black')
+
+Save_to_XLSX <- function(Table2Save, TabName, FileName)
+{
+  # Format some of the RTS Table beforehand.
+  NumRows <- nrow(Table2Save)
+  NumCols <- ncol(Table2Save)
+  
+  # Write the RTS Table into a worksheet.
+  RTSBook <- createWorkbook()
+  TabName <- paste0(TabName)
+  addWorksheet(RTSBook, TabName)
+  writeDataTable(RTSBook, TabName, Table2Save, tableStyle = 'none')
+  
+  # Some formatting afterwards.
+  addStyle(RTSBook, TabName, style = Style0, rows = 1:NumRows+1, cols = match('acct', names(Table2Save)), gridExpand = TRUE, stack = TRUE)
+  addStyle(RTSBook, TabName, style = StyleH, rows = 1, cols = 1:NumCols, gridExpand = TRUE, stack = TRUE)
+  
+  # Auto-width everything
+  setColWidths(RTSBook, TabName, 1:NumCols, widths = 'auto', ignoreMergedCells = TRUE)
+  
+  # Freeze Panes
+  freezePane(RTSBook, TabName, firstRow = TRUE, firstCol = TRUE)
+  
+  # Write the table to an XLSX spreadsheet
+  saveWorkbook(RTSBook, paste0(outputdirectory, outputprefix, FileName, '.xlsx'), overwrite = TRUE)
+}
+
+
+Save_to_XLSX(test, 'PWilson', 'PWilson.xlsx')
+Save_to_XLSX(DEP_dataset, 'DEP_Dataset', 'DEP_Dataset.xlsx')
